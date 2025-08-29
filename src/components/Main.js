32ffdafba66c946089fdec697bb1fba4c100aaa2
claracayres <0123m.m4567@gmail.com>
2025-08-28 22:12:18 -0400

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,9 @@
 import HeroImage from "../assets/hero-image.png";
+import card1 from "../assets/card1.jpg";
+import card2 from "../assets/card2.svg";
+import card3 from "../assets/card3.jpg";
+import moto from "../assets/moto.png";
+
 const Main = () => {
   return (
     <>
@@ -13,14 +18,99 @@ const Main = () => {
                 We are a family owned Mediterranean restaurant, focused on
                 traditional recipes served with a modern twist.
               </p>
-              <button>Reserve a Table</button>
+              <button className="reserve-button">Reserve a Table</button>
             </div>
             <div className="invisible"></div>
             <img className="hero-image" src={HeroImage} alt="Delicious food" />
           </div>
         </section>
 
-        
+        <section className="specials-section">
+          <div className="specials">
+            <div className="specials-header">
+              <h1>This Week's Specials</h1>
+              <button className="order-button">Online Order</button>
+            </div>
+
+            <div className="specials-list">
+              <div className="specials-item">
+                <div className="image-container">
+                  <img src={card1} alt="Greek Salad" />
+                </div>
+                <div className="title-food">
+                  <h2>Greek Salad</h2>
+                  <span className="price">$12.99</span>
+                </div>
+                <div className="description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+
+                <div>
+                  <button className="delivery-button">
+                    Order a delivery
+                    <span className="moto">
+                      <img src={moto} alt="Delivery Moto" />
+                    </span>
+                  </button>
+                </div>
+              </div>
+
+              <div className="specials-item">
+                <div className="image-container">
+                  <img src={card2} alt="Bruschetta" />
+                </div>
+                <div className="title-food">
+                  <h2>Bruschetta</h2>
+                  <span className="price">$5.99</span>
+                </div>
+                <div className="description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+                <div>
+                  <button className="delivery-button">
+                    Order a delivery
+                    <span className="moto">
+                      <img src={moto} alt="Delivery Moto" />
+                    </span>
+                  </button>
+                </div>
+              </div>
+
+              <div className="specials-item">
+                <div className="image-container">
+                  <img src={card3} alt="Lemon Dessert" />
+                </div>
+                <div className="title-food">
+                  <h2>Lemon Dessert</h2>
+                  <span className="price">$5.00</span>
+                </div>
+                <div className="description">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+                <div>
+                  <button className="delivery-button">
+                    Order a delivery
+                    <span className="moto">
+                      <img src={moto} alt="Delivery Moto" />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
